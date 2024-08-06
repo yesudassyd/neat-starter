8019f19954c337ee5c5eda20da2e773e6c6ed002
@yesudass <yesudass.agamworks@gmail.com>
2024-08-06 18:25:28 +0530

    doctor list

--- .eleventy.js
+++ .eleventy.js
@@ -36,7 +36,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static/img");
 
   // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico"); 
+  eleventyConfig.addPassthroughCopy("./src/public");
 
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
