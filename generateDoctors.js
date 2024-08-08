027577aeea6379894c4b8e555ca918ca9ca90291
@yesudass <yesudass.agamworks@gmail.com>
2024-08-08 18:50:37 +0530

    front-doctor

--- generateDoctors.js
+++ generateDoctors.js
@@ -14,23 +14,21 @@ fetch("https://66a8d49ee40d3aa6ff5996ab.mockapi.io/doctors")
     })
     .then(data => {
         data.forEach(doctor => {
-            const mdContent = `
-        ---
-        title: ${doctor.name}
-        id: ${doctor.id}
-        createdAt: ${doctor.createdAt}
-        ---
-
-        # ${doctor.name}
-
-        - **ID**: ${doctor.id}
-        - **Created At**: ${doctor.createdAt}
-            `;
+            const mdContent = 
+`---
+layout: doctor
+title: ${doctor.name}
+id: ${doctor.id}
+createdAt: ${doctor.createdAt}
+---`;
 
             const fileName = `${doctor.name.replace(/\s+/g, '_').toLowerCase()}.md`;
             const filePath = path.join(outputDir, fileName);
-
-            fs.writeFileSync(filePath, mdContent.trim());
-            console.log(`Created ${filePath}`);
+            //console.log(filePath)
+            //console.log(fs.existsSync(filePath))
+            if(!fs.existsSync(filePath)){
+                fs.writeFileSync(filePath, mdContent.trim());
+                console.log(`Created ${filePath}`);
+            }
         });
 }).catch(error => console.log(error))
